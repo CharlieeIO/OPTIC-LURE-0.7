ce3eb1f47348876fbee1cacda215c0a9fcdc4f04
Charlieesp <68710888+charlieessex@users.noreply.github.com>
2024-07-11 17:44:38 -0400

    Runplan.cpp

--- Runplan.cpp
+++ Runplan.cpp
@@ -5,7 +5,9 @@ unsigned long lastLEDUpdateMillis = 0; // Variable to keep track of the last tim
 unsigned long lastFlashCheckMillis = 0; // Variable to keep track of the last time checkAndFlashLEDs() was called
 const unsigned long flashInterval = 1000; // Interval for checking and flashing LEDs (1 seconds)
 bool flashingActive = false; // Variable to keep track of the flashing 
-
+unsigned long previousMillis = 0;
+unsigned long functionDuration = 5000;  // 15 seconds in milliseconds
+int currentPattern = 0;
 
 void LEDTrigger() {
   FetchRtcData();  // Ensure the timeinfo is updated
@@ -42,4 +44,62 @@ void LEDTrigger() {
   }
 }
 
+void Testingplan(){
+// Check if it's time to move to the next pattern
+  unsigned long currentMillis = millis();
 
+  if (currentMillis - previousMillis >= functionDuration) {
+    // Reset timer
+    previousMillis = currentMillis;
+
+    // Increment pattern index
+    currentPattern = (currentPattern + 1) % 12;  // There are 7 patterns to cycle through
+
+    // Clear NeoPixels
+    strip.clear();
+    strip.show();
+  }
+
+  // Call the current pattern
+  switch (currentPattern) {
+    case 0:
+      applyPatternToNeoPixels(AllRings, STEADY, strip.Color(0, 0, 200));  // AllRings
+      break;
+    case 1:
+      applyPatternToNeoPixels(Ring_4, STEADY, strip.Color(0, 0, 200));  // Ring_4
+      break;
+    case 2:
+      applyPatternToNeoPixels(Ring_3, STEADY, strip.Color(0, 0, 200));  // Ring_3
+      break;
+    case 3:
+      applyPatternToNeoPixels(Ring_2, STEADY, strip.Color(0, 0, 200));  // Ring_2
+      break;
+    case 4:
+      applyPatternToNeoPixels(Ring_1, STEADY, strip.Color(0, 0, 200));  // Ring_1
+      break;
+    case 5:
+      applyPatternToNeoPixels(StraightLines, STEADY, strip.Color(0, 0, 200));  // StraightLines
+      break;
+    case 6:
+      applyPatternToNeoPixels(DiagonalLines, STEADY, strip.Color(0, 0, 200));  // DiagonalLines
+      break;
+    case 7:
+     applyPatternToNeoPixels(AllRings, STEADY, strip.Color(0, 0, 200));  // DiagonalLines
+     break;
+    case 8:
+    applyPatternToNeoPixels(AllRings, BLINK, strip.Color(0, 0, 200));  // DiagonalLines
+    break;
+    case 9:
+    applyPatternToNeoPixels(AllRings, PULSE, strip.Color(0, 0, 200));  // DiagonalLines
+    break;
+    case 10:
+    applyPatternToNeoPixels(AllRings, TWINKLE, strip.Color(0, 0, 200));  // DiagonalLines
+    break;
+    case 11:
+    applyPatternToNeoPixels(AllRings, SPARKLE, strip.Color(0, 0, 200));  // DiagonalLines
+    break;
+    default:
+      // Should not happen, but handle default case if necessary
+      break;
+  }
+}
